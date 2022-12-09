51f32892a6dc842caa1b415e77a9d2bae9339fac
Douglas Kodi <kodi.shimoura@gmail.com>
2022-12-08 21:25:37 -0400

    create navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
